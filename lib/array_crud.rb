af1bfe034a44a3f264c1b7c222df9fa128ded6b2
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-17 06:00:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,8 @@ def create_an_empty_array
 end
 
 def create_an_array
- create_an_array = ["lil bub"]
- create_an_array = ["grumpy cat"]
- create_an_array = ["pasheen"]
- create_an_array = ["garfield"]
- create_an_array = ["nala cat"]
+ create_an_array = [4]
+
 end
 
 def add_element_to_end_of_array(array, element)
